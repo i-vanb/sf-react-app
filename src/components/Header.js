e70b654cef414ac4c732a0d33986d9bd0cce62fc
Ivan B <ivan.giv14@gmail.com>
2021-03-13 00:00:00 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "../styles/Header.css"
+
+
+function Header() {
+    return (
+        <header>
+            Header
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "../styles/Main.css"
+
+
+function Main() {
+    return (
+        <main>
+            Main
+        </main>
+    )
+}
+
+export default Main;
